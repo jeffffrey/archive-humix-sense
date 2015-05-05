6f49296da53cfbb2f9b2784359f79b3aa139c412
JEFFREY LIU <liuch@tw.ibm.com>
2015-05-05 14:26:04 +0800

    initial check-in of controller

---
+++ controller/config.js
@@ -0,0 +1,18 @@
+
+
+
+var jeffrey_humix_config = {
+    "org" : "vlqpzn",
+    "type" : "jeffrey-humix",
+    "id" : "b827eb26fdcc",
+    "authMethod" : "token",
+    "authToken" : "P81)B0okaVLJK4z(Ju"
+};
+
+
+
+var default_humix_config = jeffrey_humix_config;
+
+
+
+module.exports = default_humix_config;
